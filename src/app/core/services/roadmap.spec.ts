2c99344f66239374ff98a6cab1fa22325b12bab6
IvanM <sirius.ivo@gmail.com>
2026-09-18 17:13:06 -0300

    feat: add Roadmap service and corresponding tests

---
+++ src/app/core/services/roadmap.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { Roadmap } from './roadmap';
+
+describe('Roadmap', () => {
+  let service: Roadmap;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(Roadmap);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
