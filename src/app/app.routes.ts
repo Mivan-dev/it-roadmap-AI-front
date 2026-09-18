f911ee124d719b72c36cf2dd4bccb134a7cb7f43
IvanM <sirius.ivo@gmail.com>
2026-09-18 17:13:16 -0300

    feat: define application routes for Home and Roadmap components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        loadComponent: () => import('./features/home/home').then(m => m.Home)
+    },
+    {
+        path:'roadmap',
+        loadComponent: () => import('./features/roadmap/roadmap').then(m => m.Roadmap)
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./features/home/home').then(m => m.Home)
+    }
+];
